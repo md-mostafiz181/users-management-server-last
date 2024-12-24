0bc88b319919579de456ab561fb63a05daea8311
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-24 11:09:20 +0600

    show users

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+const cors =require("cors")
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors())
+app.use(express.json())
+
 const users =[
-    { id:1,Name:"Mostafizur Rahman", Email: "mostafizur@gmail.com"},
-    {id:2,Name:"Zara Rahman", Email: "mostafizur@gmail.com"},
-    {id:3,Name:"Bobi Rahman", Email: "mostafizur@gmail.com"},
-    {id:4,Name:"Malaika Rahman", Email: "mostafizur@gmail.com"}
+    {id:1,name:"Mostafizur Rahman", email: "mostafizur@gmail.com"},
+    {id:2,name:"Zara Rahman", email: "mostafizur@gmail.com"},
+    {id:3,name:"Bobi Rahman", email: "mostafizur@gmail.com"},
+ 
     
 ]
 
@@ -16,6 +21,14 @@ app.get("/", (req,res)=>{
 
 app.get("/users", (req,res)=>{
     res.send(users)
+})
+
+app.post("/users", (req,res)=>{
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id =users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
 })
 
 app.listen(port, ()=>{
